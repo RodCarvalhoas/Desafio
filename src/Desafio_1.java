4270382c3c8424669b8ed7ac101b55d308411505
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2023-02-27 21:46:36 -0300

    Desafio 1

---
+++ src/Desafio_1.java
@@ -0,0 +1,17 @@
+
+public class Desafio_1 {
+
+	public static void main(String[] args) {
+
+		int indice = 13;
+		int soma = 0;
+		int k = 0;
+
+		while (k < indice) {
+			k += 1;
+			soma += k;
+		}
+		System.out.println(soma);
+	}
+
+}
